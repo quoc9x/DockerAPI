349e33dc9a921ea0dbe242137d8d1949009f27d2
cuongquocvn <67582708+cuongquocvn@users.noreply.github.com>
2020-11-24 16:44:32 +0700

    Test build Jenkins

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace DockerAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            // Test build Jenkins
             app.UseMvc();
         }
     }
