a54724262fe10de828ea0fa655fc5313fac48469
qwrite <67582708+qwrite@users.noreply.github.com>
2020-12-04 15:12:56 +0700

    Update ValuesController.cs
    Test Excluded Regions - Jenkins

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+// Test Excluded Regions - Jenkins
 namespace DockerAPI.Controllers
 {
     [Route("api/[controller]")]
