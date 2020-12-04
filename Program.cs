5e2b351b669e72ef469b6bfb2b443357f3f8444b
qwrite <67582708+qwrite@users.noreply.github.com>
2020-12-04 15:23:06 +0700

    Update Program.cs
    Test Included Regions - Jenkins

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+// Test Included Regions - Jenkins
 namespace DockerAPI
 {
     public class Program
